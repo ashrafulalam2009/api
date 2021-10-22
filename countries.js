cd96b2d011bde9b92144146a73137d1bc29e05aa
ashraful <ashrafulhuzhou@gmail.com>
2021-10-22 09:29:55 +0600

    first commit

---
+++ countries.js
@@ -0,0 +1,17 @@
+const loadCountries = ()=> {
+
+    fetch('https://restcountries.eu/rest/v2/all')
+
+
+        .then(res => res.json())
+        .then(data => console.log(data));
+
+
+}
+
+
+
+loadCountries();
+
+
+
---
+++ mealdb.js
@@ -0,0 +1,88 @@
+const searchfood = () =>{
+
+
+const searchField = document.getElementById('search-field');
+
+
+const searchText = searchField.value;
+
+// console.log(searchText);
+
+
+// clear data 
+
+searchField.value = '';
+
+// load data        
+
+const url = `https://www.themealdb.com/api/json/v1/1/search.php?s= ${searchText} ` ;
+
+
+// console.log(url);
+
+fetch(url)
+
+.then(res => res.json())
+.then(data => displaySearchResult(data.meals));
+
+}
+
+
+
+// for displaying code on website 
+
+const displaySearchResult = meals => {
+
+
+const searchResult = document.getElementById('search-result');
+
+searchResult.innerHTML = '';
+
+meals.forEach(meal => {
+
+console.log(meal);
+
+
+
+const div = document.createElement('div');
+
+div.classList.add('col');
+
+div.innerHTML = `    
+
+<div onclick=" loadMealDetail(${meal.idMeal}) " class="card h-100">
+      <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+         <h5 class="card-title">${meal.strMeal}</h5>
+         <p class="card-text">${meal.strInstructions.slice(0,100)}</p>
+
+    </div>
+</div>
+
+`;
+
+
+searchResult.appendChild(div);
+
+
+
+})
+
+}
+
+
+const loadMealDetail = mealId =>{
+
+console.log(mealId);
+
+
+const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
+
+
+fetch(url)
+.then(res =>res.json())
+.then(data => console.log(data))
+
+
+
+}
